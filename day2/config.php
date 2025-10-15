6bb09eda82ce1320ed60ca3b629a4c7c5ab2e1b9
Moktan Sujita <sujitamoktan77@gmail.com>
2025-10-15 22:35:47 +0545

    Configuration recap

---
+++ day2/config.php
@@ -0,0 +1,12 @@
+<?php
+define('BASE_URL', 'localhost/backend/config.php');
+
+try{
+$conn = new PDO("mysql:host=localhost;dbname=backend_db","root","");
+$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    die("ERROR:" . $e->getMessage());
+}
+
+?>

1a8d1a5813f4295abe5d2670c0872dc27ff4c04a
Moktan Sujita <sujitamoktan77@gmail.com>
2025-10-24 20:05:03 +0545

    Configuration with database

---
+++ day8/config.php
@@ -0,0 +1,11 @@
+<?php
+define('BASEURL','backend/day8/config.php');
+
+try{
+  $conn = new PDO ("mysql:host=localhost;dbname=backend_db","root","");
+  $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    die("Error:". $e->getMessage());
+}
+?>

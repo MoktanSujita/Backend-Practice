12707ee6bc20c844a139a6add9c7c30d4214ef0c
Moktan Sujita <sujitamoktan77@gmail.com>
2025-10-26 22:05:18 +0545

    Configuration file

---
+++ day9/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('BASEURL','config.php');
+
+try{
+
+    $conn= new PDO("mysql:host = localhost;dbname=backend_db","root","");
+    $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+}
+catch(PDOException $e){
+    die('Error:' .$e->getMessage());
+}
+?>
